097838b1503b42a6f46ce7c14d20eb510afda0da
Eamon Burns (Laptop WSL) <me@eamonburns.com>
2025-01-10 00:23:05 -0800

    feat: use config file, don't do hacky copy-to-cache thing

--- build.zig
+++ build.zig
@@ -1,18 +1,24 @@
 const std = @import("std");
+const print = std.debug.print;
 
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
 pub fn build(b: *std.Build) void {
-    const tool = b.addExecutable(.{
-        .name = "generate_module_wrapper",
-        .root_source_file = b.path("./tools/generate_module_wrapper.zig"),
-        .target = b.host,
-    });
+    const wrapper_path = createWrapperFromConfigFile(b, "./modules.json") catch |err| {
+        print("{}\n", .{err});
+        std.process.exit(1);
+    };
 
-    const tool_step = b.addRunArtifact(tool);
-    const wrapper = tool_step.addOutputFileArg("wrapper.zig");
-    tool_step.addDirectoryArg(b.path("./modules/"));
+    // const tool = b.addExecutable(.{
+    //     .name = "generate_module_wrapper",
+    //     .root_source_file = b.path("./tools/generate_module_wrapper.zig"),
+    //     .target = b.host,
+    // });
+    //
+    // const tool_step = b.addRunArtifact(tool);
+    // const wrapper = tool_step.addOutputFileArg("wrapper.zig");
+    // tool_step.addDirectoryArg(b.path("./modules/"));
 
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
@@ -34,7 +40,7 @@ pub fn build(b: *std.Build) void {
     });
 
     exe.root_module.addAnonymousImport("wrapper", .{
-        .root_source_file = wrapper,
+        .root_source_file = wrapper_path,
     });
 
     b.installArtifact(exe);
@@ -86,4 +92,111 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
+}
+
+const ModConf = struct {
+    module_dir_path: ?[]const u8,
+    modules: []const []const u8,
+};
+
+fn createWrapperFromConfigFile(b: *std.Build, conf_path: []const u8) !std.Build.LazyPath {
+    const cwd = std.fs.cwd();
+    // Read and parse config file
+    const conf_file = cwd.openFile(conf_path, .{}) catch |err| switch (err) {
+        error.FileNotFound => {
+            print("error: config file not found: {s}\n", .{conf_path});
+            std.process.exit(1);
+        },
+        else => {
+            print("error: {}\n", .{err});
+            std.process.exit(1);
+        },
+    };
+
+    const conf_bytes = try conf_file.readToEndAlloc(b.allocator, std.math.maxInt(usize));
+
+    const conf_parsed = try std.json.parseFromSlice(ModConf, b.allocator, conf_bytes, .{});
+    defer conf_parsed.deinit();
+
+    // Create output directories
+    try cwd.deleteTree(".generated/");
+    const generated_dir = try cwd.makeOpenPath(".generated/", .{});
+    const generated_module_dir = try generated_dir.makeOpenPath("modules/", .{});
+
+    // Copy module source files to module dir
+
+    const conf_module_dir_path = conf_parsed.value.module_dir_path orelse "./modules/";
+
+    const conf_module_dir = cwd.openDir(
+        conf_module_dir_path,
+        .{ .iterate = true },
+    ) catch |err| switch (err) {
+        error.FileNotFound => {
+            print("error: module directory not found: {s}\n", .{conf_module_dir_path});
+            std.process.exit(1);
+        },
+        else => {
+            print("error: {}\n", .{err});
+            std.process.exit(1);
+        },
+    };
+
+    for (conf_parsed.value.modules) |mod| {
+        conf_module_dir.copyFile(mod, generated_module_dir, mod, .{}) catch |err| switch (err) {
+            error.FileNotFound => {
+                print("error: could not find module {s} in module directory {s}\n", .{ mod, conf_module_dir_path });
+                std.process.exit(1);
+            },
+            else => {
+                print("error: {}\n", .{err});
+                std.process.exit(1);
+            },
+        };
+    }
+
+    // Generate wrapper module
+
+    const wrapper_path = ".generated/wrapper.zig";
+
+    const wrapper_file = try cwd.createFile(wrapper_path, .{});
+
+    _ = try wrapper_file.write(
+        \\pub const Mod = struct {
+        \\    doThing: *const fn() void,
+        \\};
+        \\
+        \\const mod_types = [_]type{
+        \\
+    );
+
+    for (conf_parsed.value.modules) |mod| {
+        try std.fmt.format(
+            wrapper_file.writer(),
+            "    @import(\"./modules/{s}\"),\n",
+            .{mod},
+        );
+    }
+    _ = try wrapper_file.write("};\n\n");
+
+    _ = try wrapper_file.write(
+        \\pub const modules = blk: {
+        \\    var mods: [mod_types.len]Mod = undefined;
+        \\    for (mod_types, 0..) |mod, i| {
+        \\        mods[i] = Mod{
+        \\            .doThing = &mod.doThing,
+        \\        };
+        \\    }
+        \\    break :blk mods;
+        \\};
+        \\
+    );
+
+    return b.path(wrapper_path);
+}
+
+fn contains(slice: []const []const u8, str: []const u8) bool {
+    for (slice) |v| {
+        if (v == str) return true;
+    }
+    return false;
 }
--- src/main.zig
+++ src/main.zig
@@ -3,19 +3,8 @@ const print = std.debug.print;
 
 const modules = @import("wrapper").modules;
 
-const Mod = struct {
-    doThing: *const fn () void,
-};
-
 pub fn main() !void {
-    var mods: [modules.len]Mod = undefined;
-    inline for (modules, 0..) |mod, i| {
-        mods[i] = Mod{
-            .doThing = &mod.doThing,
-        };
-    }
-
-    for (mods) |mod| {
+    for (modules) |mod| {
         mod.doThing();
     }
 }
